caa836fdb3f6e025ad5aef860e2b9bc0281afee3
Peer Naupold <greenion@gmx.de>
2020-02-13 02:38:52 +0100

    debug=False

--- app.py
+++ app.py
@@ -422,4 +422,4 @@ def insert_rating(recipe_id, recipe_title):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT', 5000)),
-            debug=True)
+            debug=False)
